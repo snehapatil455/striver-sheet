8828cc1cb6baed5313558bdb509a9b259950dfaa
snehapatil455 <snehapatil6477@gmail.com>
2022-06-19 10:23:38 +0530

    //

---
+++ Day3/search2Dmatrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+     
+        if(!matrix.size()) return false;
+        
+        int n =matrix.size();    //row
+        int m =matrix[0].size(); //column
+        
+        int low=0;
+        int high=(n*m)-1;
+        
+        while(low<=high)
+        {
+           int  mid=(low + (high-low)/2);
+            if(matrix[mid/m][mid%m]==target){
+                  return true; 
+            }
+               
+            if(matrix[mid/m][mid%m]<target)
+            {
+                 low=mid+1;
+            }
+               
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+};
