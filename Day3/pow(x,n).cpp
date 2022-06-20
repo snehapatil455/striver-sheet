1e2a9a447de30c54403d7031023b9d1049ae3212
snehapatil455 <snehapatil6477@gmail.com>
2022-06-20 10:22:23 +0530

    //

--- Day3/pow(x,n).cpp
+++ Day3/pow(x,n).cpp
@@ -2,4 +2,5 @@
 //2^10=(2*2)^5= 4^5(ans)
 //4^5= as power is odd we can write it as 4*4^4(so let's find ans for 4^4)  ---------eq(1)
 //4^4= as power is even we can simply do our operations like (4*4)^2= 16^2 [(x*x)^n/2]
-//16^2=(16*16)^1= 256 (now put this value )
+//16^2=(16*16)^1= 256 (now put this value in eq(1))
+//4*256= 1024
